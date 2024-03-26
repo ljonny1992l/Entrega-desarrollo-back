fa862909415759daf75684e1352aa8f47eabfa11
jonathan fernandez <jonnyfernandez1992@gmail.com>
2024-03-26 11:25:20 -0300

    subiendo proyecto

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const ProductManager = require("./ProductManager");
 const productMngr = new ProductManager();
 productMngr.addProduct("F.PH", "Fideo puro huevo", "Aceite de oliva", "ajo" ,"Albaca", 3500, "urlImage", 10);
 productMngr.addProduct("Canelones", "canelones", "Masa de panqueque", "salsa de tomate", "Queso Reggianito", 5000, "urlImage", 10);
-productMngr.addProduct("Lasagna", "Jamon y Queso", "Masa puro huevo, salsa de tomate" , "mozzarella"," morron", 6500, "urlImage", 10);
+productMngr.addProduct("Lasagna", "Jamon y Queso", "Masa puro huevo, salsa de tomate" , "Mozzarella","Reggianito", 6500, "urlImage", 10);
 
 console.log("---getProducts()---")
 productMngr.getProducts();
